72a4e5e5e194b7ba91238cafdb9972708927a967
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-17 14:26:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,32 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.vpc_id
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Change to your IP for security
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "EC2-SG" }
+}
+
+
 resource "aws_instance" "nginx_server" {
   ami             = var.ami_id
   instance_type   = var.instance_type
@@ -45,30 +74,3 @@ resource "aws_instance" "nginx_server" {
 EOF
 }
 
-resource "aws_security_group" "ec2_sg" {
-  vpc_id = var.vpc_id
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Change to your IP for security
-  }
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "EC2-SG" }
-}
